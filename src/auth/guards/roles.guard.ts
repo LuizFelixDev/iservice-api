40d0ee07168050ba6c541db5ac8886099cdde3c8
Eduardo-SNantos <eduardoshw123@gmail.com>
2026-04-29 16:58:26 -0300

    fix: tipa request e user no RolesGuard

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -5,8 +5,17 @@ import {
   ForbiddenException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
 import { ROLES_KEY } from 'src/auth/decorators/roles.decorator';
 import { RoleName } from 'src/roles/enums/role.enum';
+
+interface UserPayload {
+  roles: RoleName[];
+}
+
+interface RequestWithUser extends Request {
+  user: UserPayload;
+}
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -15,17 +24,14 @@ export class RolesGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<RoleName[]>(
       ROLES_KEY,
-      [
-        context.getHandler(),
-        context.getClass(),
-      ],
+      [context.getHandler(), context.getClass()],
     );
 
     if (!requiredRoles) {
       return true;
     }
 
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
     const user = request.user;
 
     if (!user) {
