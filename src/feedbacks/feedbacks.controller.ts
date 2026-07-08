f3a002f7ad4820f206dc26efc40ca9802f15ca62
Ismael Gomes <ismaelcraft74@gmail.com>
2026-07-07 21:07:23 -0300

    fix: resolver erro de tipagem apontado pelo linter no FeedbacksController

--- src/feedbacks/feedbacks.controller.ts
+++ src/feedbacks/feedbacks.controller.ts
@@ -3,13 +3,22 @@ import { FeedbacksService } from './feedbacks.service';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
+interface RequestWithUser {
+  user: {
+    id: string;
+  };
+}
+
 @Controller('feedbacks')
 export class FeedbacksController {
   constructor(private readonly feedbacksService: FeedbacksService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post()
-  async create(@Req() req: any, @Body() createFeedbackDto: CreateFeedbackDto) {
+  async create(
+    @Req() req: RequestWithUser,
+    @Body() createFeedbackDto: CreateFeedbackDto,
+  ) {
     const userId = req.user.id;
     return await this.feedbacksService.create(userId, createFeedbackDto);
   }
